204c191e314c70026f33e69086732567e70c296c
s18665 <61874805+s18665@users.noreply.github.com>
2020-12-16 00:50:55 +0100

    Update 7b.js

--- project7/7b.js
+++ project7/7b.js
@@ -1,20 +1,27 @@
-const fs = require('fs');
+function CheckDir(Fpath)
+{
+    const fs = require('fs');
+
+
+
+    fs.watch(Fpath, "utf-8",function (event,trigger)
+    {
+        console.log("There was a "+event+" on a: "+trigger);
+        if(String(event)==="change")
+        {
+            var tempfile=Fpath+"\\"+trigger;
+            fs.readFile(tempfile, 'utf8', function (err,data) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(data);
+            });
+
+        }
+
+    })
+}
+
 const Fpath="C:\\Users\\PC\\Pictures";
 
-
-fs.watch(Fpath, "utf-8",function (event,trigger)
-{
-    console.log("There was a "+event+" on a: "+trigger);
-    if(String(event)==="change")
-    {
-        var tempfile=Fpath+"\\"+trigger;
-        fs.readFile(tempfile, 'utf8', function (err,data) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log(data);
-        });
-
-    }
-
-})
+CheckDir(Fpath);
